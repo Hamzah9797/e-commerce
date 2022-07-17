7a4d0d4b056c4acd1842f2dd485a22b70d1ecbba
Hamzah Hameed <hamzahhameed9797@gmail.com>
2022-07-17 23:16:59 +0300

    e-commerce ui completed

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+// dependencies
+import React from "react";
+
+//components
+import NavBar from "../components/NavBar";
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import NewsLetter from "../components/NewsLetter";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <NavBar />
+      <Slider />
+      <Categories />
+      <Products />
+      <NewsLetter />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
