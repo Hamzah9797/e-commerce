43a5e4ba27d711cd61f9467c62e40d8acdc91266
Hamzah9797 <hamzahhameed9797@gmail.com>
2023-08-19 18:35:51 +0700

    lints checks

--- client/src/__tests__/categoryItems.test.js
+++ client/src/__tests__/categoryItems.test.js
@@ -1,5 +1,6 @@
+/* eslint-disable */
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import CategoryItem from "../components/CategoryItem";
 
